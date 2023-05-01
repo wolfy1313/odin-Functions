1ab82d306aecc73018c72fef4070547bf46e5e8f
jeremyHarrell <hillvulf@gmail.com>
2023-05-01 11:45:30 -0500

    complete mdn lesson on split and template literals

--- python.py
+++ python.py
@@ -1,12 +1,30 @@
-x = 0;
-y = 0;
+# x = 0;
+# y = 0;
 
-sum = 0;
+# sum = 0;
 
-x = input("X: ")
-y = input("Y: ")
+# x = input("X: ")
+# y = input("Y: ")
 
-sum = int(x) + int(y)
+# sum = int(x) + int(y)
 
-print("Answer is: ")
-print(sum)
+# print("Answer is: ")
+# print(sum)
+
+correct = "IndyPy"
+tries = 0
+keepGoing = True
+
+while keepGoing:
+    tries += 1
+    print("Try #", tries)
+
+    guess = input("What is the password? ")
+    if guess == correct:
+        print("That's right")
+        keepGoing = False
+    elif tries >= 3:
+        print("Too many tries, bye bye!")
+        keepGoing = False
+    else:
+        print("Invalid password. Try again.")
